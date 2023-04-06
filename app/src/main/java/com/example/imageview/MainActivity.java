f8cf520643031a4c6407debd019ebb3b79f9f8a1
kbru gedfie <kbrugedfie@gmail.com>
2023-04-06 10:05:13 -0700

    initial commit

---
+++ app/src/main/java/com/example/imageview/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.imageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FloatingActionButton fab=(FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),orderActivity.class);
+                startActivities(new  Intent[]{intent});
+
+            }
+        });
+
+
+    }
+    public void displayToast(String message){
+        Toast.makeText(getApplicationContext(),message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void showDonutOrder(View view) {
+        displayToast(getString(R.string.donut_order_message));
+    }
+
+    public void showIceCreamOrder(View view) {
+        displayToast(getString(R.string.ice_cream_order_message));
+    }
+
+    public void showFroyoOrder(View view) {
+        displayToast(getString(R.string.froyo_order_message));
+}
+    }
---
+++ app/src/main/java/com/example/imageview/orderActivity.java
@@ -0,0 +1,63 @@
+package com.example.imageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class orderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        Spinner spinner=findViewById(R.id.spinner_label);
+        if(spinner!=null){
+            spinner.setOnItemSelectedListener(this);
+        }
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.labels_array, android.R.layout.simple_spinner_item);
+        if(spinner!=null){
+            spinner.setAdapter(adapter);
+        }
+
+    }
+    public void displayToast(String message)
+    {
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked=((RadioButton) view).isChecked();
+        switch(view.getId()){
+            case R.id.sameday:
+                if(checked){
+                    displayToast(getString(R.string.same_day_messenger_service));
+                }
+            case R.id.nextday:
+                if (checked){
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                }
+            case R.id.pickup:
+                if (checked){
+                    displayToast(getString(R.string.pick_up));
+                }
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String spinnerLable1=adapterView.getItemAtPosition(i).toString();
+        displayToast(spinnerLable1);
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
